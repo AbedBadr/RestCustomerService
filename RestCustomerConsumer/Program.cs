9b299cddc5d02acfba910cf61632d6a972277475
AbxdHxcks <abedbadr1999@hotmail.com>
2019-09-30 10:29:21 +0200

    Add project files.

---
+++ RestCustomerConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestCustomerConsumer
+{
+    class Program
+    {
+        private static string CustomersUri = "https://localhost:44329/api/customer/";
+        static void Main(string[] args)
+        {
+        }
+
+        public static async Task<IList<Customer>> GetCustomersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(CustomersUri);
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
+        }
+
+    }
+}
---
+++ RestCustomerService/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCustomerService.Model;
+
+namespace RestCustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public static List<Customer> cList = new List<Customer>();
+        public static int nextid = 0;
+
+        // GET: api/Customer
+        [HttpGet]
+        public List<Customer> Get()  // or public IEnumerable<string> Get()
+        {
+            return cList;
+        }
+
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
