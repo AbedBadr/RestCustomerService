0aa37abb4c16476679eaeaaa1fbef5c02af44ad9
AbxdHxcks <abedbadr1999@hotmail.com>
2019-10-07 12:19:47 +0200

    Added HTTpRequests mehod that calls all http request methods

--- RestCustomerConsumer/Program.cs
+++ RestCustomerConsumer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -12,6 +13,12 @@ namespace RestCustomerConsumer
         private static string CustomersUri = "https://localhost:44329/api/customer/";
         static void Main(string[] args)
         {
+            HttpRequests();
+        }
+
+        public static async void HttpRequests()
+        {
+            Console.WriteLine("GET REQUEST");
             IList<Customer> cIList = GetCustomersAsync().Result;
 
             foreach (Customer customer in cIList)
@@ -19,10 +26,23 @@ namespace RestCustomerConsumer
                 Console.WriteLine(customer);
             }
 
-            Console.WriteLine();
+            Console.WriteLine("GET SPECIFIC REQUEST");
             
             Customer getCustomer = GetCustomerAsync(1).Result;
             Console.WriteLine(getCustomer);
+
+            Console.WriteLine("POST REQUEST");
+            
+            Customer postCustomer = new Customer(3, "GangLand", "Mahurti", 1995);
+            await PostCustomerAsync(postCustomer);
+            
+            List<Customer> listAfterPost = (List<Customer>)GetCustomersAsync().Result;
+            foreach (Customer customer in listAfterPost)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("PUT REQUEST");
         }
 
         public static async Task<IList<Customer>> GetCustomersAsync()
@@ -45,10 +65,15 @@ namespace RestCustomerConsumer
             }
         }
 
-        public static async void PostCustomerAsync(Customer customer)
+        public static async Task PostCustomerAsync(Customer customer)
         {
             using (HttpClient client = new HttpClient())
             {
+                string body = JsonConvert.SerializeObject(customer);
+
+                StringContent content = new StringContent(body, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage result = await client.PostAsync(CustomersUri, content);
             }
         }
     }
