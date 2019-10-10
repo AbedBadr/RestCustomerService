15115999ea971c7998c9d1852a5952bd87016ebc
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-10-10 20:11:28 +0200

    Added support for CORS

--- RestCustomerService/Startup.cs
+++ RestCustomerService/Startup.cs
@@ -26,6 +26,7 @@ namespace RestCustomerService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,12 @@ namespace RestCustomerService
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin().AllowAnyMethod();
+                // allow everything from anywhere
+            });
             app.UseMvc();
         }
     }
