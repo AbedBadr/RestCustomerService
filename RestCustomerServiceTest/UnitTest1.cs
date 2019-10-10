9f14863c776ed54134d6f50eaf3f4b95c839f8d4
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-10-10 20:17:17 +0200

    Added new UnitTest project for RestCustomerService

---
+++ RestCustomerServiceTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestCustomerServiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
